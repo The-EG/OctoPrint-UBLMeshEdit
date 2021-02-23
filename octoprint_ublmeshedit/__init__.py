1b45172588e2ce4fe56203d2d106bbc013816e7e
Taylor Talkington <taylor.talkington@gmail.com>
2021-02-23 14:06:09 -0500

    initial commit

---
+++ octoprint_ublmeshedit/__init__.py
@@ -0,0 +1,139 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+### (Don't forget to remove me)
+# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
+# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
+# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
+# as necessary.
+#
+# Take a look at the documentation on what other plugin mixins are available.
+
+import octoprint.plugin
+
+class UBLMeshEditPlugin(octoprint.plugin.SettingsPlugin,
+                        octoprint.plugin.AssetPlugin,
+                        octoprint.plugin.TemplatePlugin):
+
+	def __init__(self):
+		self.wait_mesh = False
+		self.mesh_data = None
+		self.in_topo = False
+		self.slot_num = None
+
+	##~~ SettingsPlugin mixin
+
+	def get_settings_defaults(self):
+		return dict(
+			# put your plugin's default settings here
+		)
+
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		# Define your plugin's asset files to automatically include in the
+		# core UI here.
+		return dict(
+			js=["js/ublmeshedit.js"],
+			css=["css/ublmeshedit.css"]
+		)
+
+	def on_gcode_sending(self, comm, phase, cmd, cmd_type, gcode, subcode=None, tags=None, *args, **kwargs):
+		if cmd=='M420 V1 T1': self.wait_mesh = True
+		return None
+
+	def on_gcode_recieved(self, comm, line, *args, **kwargs):
+		if not self.wait_mesh or line.strip() in ['','wait','Not SD printing'] or line.strip()[:2]=='T:':
+			return line
+		
+		if line.strip() == 'Bed Topography Report for CSV:':
+			self.in_topo = True
+			self.mesh_data = []
+		elif line.strip() in ['Mesh is valid', 'Mesh is not valid']:
+			self.in_topo = False
+		elif line.strip() == 'ok':
+			self.wait_mesh = False
+			self.send_mesh_collected_event()
+		elif 'Storage slot:' in line.strip():
+			self.slot_num = int(line.strip()[13:])
+		elif self.in_topo:
+			self.mesh_data.append(list(map(float,line.strip().split('\t'))))
+
+		return line
+
+	def on_atcommand_sending(self, comm, phase, cmd, params, tags=None, *args, **kwargs):
+		if cmd == 'UBLMESHEDIT': self.wait_mesh = True
+
+	def send_mesh_collected_event(self):
+		event = octoprint.events.Events.PLUGIN_UBLMESHEDIT_MESH_READY
+		if self.mesh_data is None:
+			data = {'result': 'no mesh'}
+		else:
+			data = {'result': 'ok', 'data': self.mesh_data, 'gridSize': len(self.mesh_data), 'saveSlot': self.slot_num}
+		self._event_bus.fire(event, payload=data)
+
+	def register_custom_events(*args, **kwargs):
+		return ["mesh_ready"]
+
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		# Define the configuration for your plugin to use with the Software Update
+		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+		# for details.
+		return dict(
+			ublmeshedit=dict(
+				displayName="UBL Mesh Editor",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="The-EG",
+				repo="OctoPrint-UBLMeshEdit",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/The-EG/OctoPrint-UBLMeshEdit/archive/{target_version}.zip",
+
+				# release channels
+				stable_branch=dict(
+					name="Stable",
+					branch="main",
+					comittish=["main"]
+				),
+				prerelease_branches=[
+					{
+						"name": "Release Candidate",
+						"branch": "rc",
+						"committish": ["rc", "main"]
+					}
+				]
+			)
+		)
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+__plugin_name__ = "UBL Mesh Editor"
+
+# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
+# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
+# compatibility flags according to what Python versions your plugin supports!
+#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
+#__plugin_pythoncompat__ = ">=3,<4" # only python 3
+__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = UBLMeshEditPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+		"octoprint.comm.protocol.gcode.received": __plugin_implementation__.on_gcode_recieved,
+		"octoprint.comm.protocol.gcode.sending": __plugin_implementation__.on_gcode_sending,
+		"octoprint.comm.protocol.atcommand.sending": __plugin_implementation__.on_atcommand_sending,
+		"octoprint.events.register_custom_events": __plugin_implementation__.register_custom_events
+	}
+
---
+++ octoprint_ublmeshedit/static/js/ublmeshedit.js
@@ -0,0 +1,139 @@
+/*
+ * View model for OctoPrint-UBLMeshEdit
+ *
+ * Author: Taylor Talkington
+ * License: AGPLv3
+ */
+$(function() {
+    function UblmesheditViewModel(parameters) {
+        var self = this;
+
+        self.settings = parameters[0];
+        self.printerState = parameters[1];
+
+        self.pointValue = ko.observable(undefined);
+        self.pointCol = ko.observable(undefined);
+        self.pointRow = ko.observable(undefined);
+        self.gridSize = undefined;
+        self.gridData = undefined;
+        self.saveSlot = ko.observable(undefined);
+
+        self.meshButtonColor = function(value, min, max) {
+            var minColor = [79, 91, 249];
+            var zeroColor = [87, 150, 67];
+            var maxColor = [186, 50, 50];
+            if (value == min) return `rgb(${minColor[0]}, ${minColor[1]}, ${minColor[2]})`;
+            if (value == max) return `rgb(${maxColor[0]}, ${maxColor[1]}, ${maxColor[2]})`;
+            if (value == 0) return `rgb(${zeroColor[0]}, ${zeroColor[1]}, ${zeroColor[2]})`;
+
+            var lowColor;
+            var hiColor;
+            var s;
+            
+            if (value < 0) {
+                lowColor = minColor;
+                hiColor = zeroColor;
+                s = (value - min) / (min * -1);
+            } else {
+                lowColor = zeroColor;
+                hiColor = maxColor;
+                s = value / max;
+            }
+
+            var r = lowColor[0] + ((hiColor[0] - lowColor[0]) * s);
+            var g = lowColor[1] + ((hiColor[1] - lowColor[1]) * s);
+            var b = lowColor[2] + ((hiColor[2] - lowColor[2]) * s);
+
+            return `rgb(${r}, ${g}, ${b})`;
+        }
+
+        self.onEventplugin_ublmeshedit_mesh_ready = function(payload) {
+
+            $('#ublMeshEditorGrid').empty();
+
+            self.pointRow(undefined);
+            self.pointCol(undefined);
+            self.pointValue(undefined);
+
+            if (payload.result!='ok') {
+                self.gridSize = undefined;
+                self.gridData = undefined;
+                self.saveSlot(undefined);
+                return;
+            }
+
+            self.gridSize = payload.gridSize;
+            self.gridData = payload.data;
+
+            self.saveSlot(payload.saveSlot);
+
+            var tbl = $('<table class="mesh-grid" />').appendTo("#ublMeshEditorGrid");
+
+            var valMin = 0;
+            var valMax = 0;
+            for(var row = 0; row < self.gridSize; row++) {
+                for(var col =0; col < self.gridSize; col++) {
+                    if (self.gridData[row][col] < valMin) valMin = self.gridData[row][col];
+                    if (self.gridData[row][col] > valMax) valMax = self.gridData[row][col];
+                }
+            }
+ 
+            for (var row = 0; row < self.gridSize; row++) {
+                var tr = $("<tr />");
+                tbl.append(tr);
+                for (var col = 0; col < self.gridSize; col++) {
+                    var  btn = $('<button class="mesh-button" />');
+                    btn.text(self.gridData[row][col].toFixed(3));
+                    btn.attr({'data-col': col, 'data-row': self.gridSize - 1 - row, 'style': `background-color: ${self.meshButtonColor(self.gridData[row][col],valMin, valMax)}`});
+                    btn.click(self.selectPoint)
+                    var td = $('<td />');
+                    td.append(btn);
+                    tr.append(td);
+                }
+            }
+        }
+
+        self.getMesh = function() {
+            OctoPrint.control.sendGcode("M420 V1 T1");
+        }
+
+        self.selectPoint = function(event) {
+            self.pointCol(parseInt($(event.target).attr('data-col')));
+            self.pointRow(parseInt($(event.target).attr('data-row')));
+            self.pointValue(parseFloat($(event.target).text()));
+            $('#tab_plugin_ublmeshedit button.mesh-button').removeClass('mesh-selected');
+            $(event.target).toggleClass('mesh-selected');
+            $('#ublMeshEditSavePoint').prop('disabled', false);
+        }
+
+        self.zeroMesh = function() {
+            OctoPrint.control.sendGcode('G29 P0');
+            self.getMesh();
+        }
+
+        self.savePoint = function() {
+            OctoPrint.control.sendGcode(`M421 I${self.pointCol()} J${self.pointRow()} Z${self.pointValue()}`);
+            self.getMesh();
+        }
+
+        self.saveToSlot = function() {
+            OctoPrint.control.sendGcode(`G29 S${self.saveSlot()}`);
+            self.getMesh();
+        }
+
+        self.loadFromSlot = function() {
+            OctoPrint.control.sendGcode(`G29 L${self.saveSlot()}`);
+            self.getMesh();
+        }
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: UblmesheditViewModel,
+        dependencies: ["settingsViewModel" , "printerStateViewModel"],
+        elements: ["#tab_plugin_ublmeshedit"]
+    });
+});
