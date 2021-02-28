0a4063088d47f9edc96e0073ab15d9a88f3a1f0a
Taylor Talkington <taylor.talkington@gmail.com>
2021-02-28 09:35:37 -0500

    Update description in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = """A plugin that provides a method for manually editing and saving/loading UBL meshes."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Taylor Talkington"
